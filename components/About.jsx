e7b07e6d51a8e827c2a8fd24ddbeee171e206af7
SebastianCamargoGarcia <ssebastiancamargo19@gmail.com>
2024-11-20 14:06:40 -0500

    Agrego la experiencia en ADUR

--- components/About.jsx
+++ components/About.jsx
@@ -7,7 +7,27 @@ const About = () => {
             <div className='col-span-2'>
                 <p className='uppercase text-xl tracking-widest text-[#5651e5] '>¡Sobre mi!</p>
                 <p className='py-2 text-gray-600'>
-                  Trabajé en <span style={{ color: '#11A0D9' }}>NEDUGA TECH</span> brindando soporte técnico especializado a
+                Actualmente me encuentro trabajando como Desarrollador Junior para <a 
+                                                                                    href="https://www.adur.com/" 
+                                                                                    style={{ color: '#11A0D9' }}
+                                                                                    target="_blank" 
+                                                                                    rel="noopener noreferrer"
+                                                                                  >
+                                                                                    Adur Software Productions / Transkal
+                                                                                  </a> en la que implemento soluciones personalizadas en el ERP Transkal. En mi rol actual, diseño y desarrollo
+                  funcionalidades específicas para optimizar procesos empresariales, trabajando de manera eficiente con equipos multidisciplinarios para mejorar la integración y
+                  funcionalidad del sistema. Mi enfoque se centra en la calidad del código y en el cumplimiento de los requisitos del cliente, contribuyendo activamente a
+                  proyectos que potencian la eficiencia operativa.  
+                  <br/>
+                  <br/>
+                  Trabajé en <a 
+                              href="https://nedugatech.com/" 
+                              style={{ color: '#11A0D9' }}
+                              target="_blank" 
+                              rel="noopener noreferrer"
+                              >
+                              NEDUGA TECH
+                              </a> brindando soporte técnico especializado a
                   usuarios de SAP B1, resolviendo incidencias y proporcionando soluciones eficientes para garantizar la continuidad operativa,
                   asimismo, realice restricciones personalizadas en la base de datos para garantizar la integridad y coherencia de los datos,
                   incluyendo validaciones de campos obligatorios y reglas de negocio utilizando Stored Procedures (SP) en SQL Server para adaptar
@@ -16,6 +36,8 @@ const About = () => {
                   También he adquirido habilidades valiosas a través de proyectos académicos y personales, demostrando mi capacidad para abordar
                   desafíos técnicos y colaborar en equipos multidisciplinarios. Estos proyectos han fortalecido mis habilidades en desarrollo de software,
                   trabajo en equipo y resolución de problemas, preparándome para enfrentar desafíos en el campo laboral.
+                  
+                  
                 </p>
             </div>
             <div className='w-full h-auto m-auto shadow-xl shadow-blue-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
